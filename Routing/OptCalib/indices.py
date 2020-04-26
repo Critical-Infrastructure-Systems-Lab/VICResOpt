1f562e8a4bd3560c974b81f5a6007cea4b42e505
Thanh Dang <thanhiwer@gmail.com>
2020-04-26 17:26:43 +0800

    Update indice calculation

--- Routing/OptCalib/indices.py
+++ Routing/OptCalib/indices.py
@@ -6,13 +6,13 @@ def NSE(obs,mol):
     meangauge = 0
     count = 0
     for i in range(len(obs)):
-        if (mol[i]>=0):
+        if (obs[i]>=0):
             numerator+=pow(abs(mol[i])-obs[i],2)
             meangauge+=obs[i]
             count+=1
     meangauge=meangauge/count
     for i in range(len(obs)):
-        if (mol[i]>=0):
+        if (obs[i]>=0):
             denominator+=pow(obs[i]-meangauge,2)
     return 1-numerator/denominator
 
@@ -21,7 +21,7 @@ def TRMSE(obs,mol):
     trmse = 0
     count = 0
     for i in range(len(obs)):
-        if (mol[i]>=0):
+        if (obs[i]>=0):
             zmdl = (pow((abs(mol[i])+1),0.3)-1)/0.3
             zobs = (pow((abs(obs[i])+1),0.3)-1)/0.3
             trmse+=pow(zmdl-zobs,2)
@@ -33,7 +33,7 @@ def MSDE(obs,mol):
     msde = 0
     count = 0
     for i in range(1,len(obs)):
-        if ((mol[i]>=0) and (mol[i-1]>=0)):
+        if ((obs[i]>=0) and (obs[i-1]>=0)):
             msde+=pow(((obs[i]-obs[i-1])-(mol[i]-mol[i-1])),2)
             count+=1
     return msde/count
@@ -44,7 +44,7 @@ def ROCE(obs,mol):
     sumobs = 0
     count = 0
     for i in range(len(obs)):
-        if (mol[i]>=0):
+        if (obs[i]>=0):
             summdl+=mol[i]
             sumobs+=obs[i]
             count+=1
